9be6bc1a335b6f344d65eea72c7c36795365f094
MiguelJardim <migueljardim2000@gmail.com>
2020-10-02 16:41:47 +0100

    udp

--- src/pd.c
+++ src/pd.c
@@ -50,6 +50,71 @@ int main(int argc, char **argv) {
         }
     }
 
+    char aux[9];
+    int pos = 0;
+
+    // read reg
+    while ((c = getchar()) != ' ') {
+        aux[pos++] = c;
+    }
+    aux[pos++] = '\0';
+    char reg[4] = "reg";
+    
+    if (strcmp(aux, reg) != 0) {
+        fprintf(stderr, "invalid input");
+        exit(EXIT_FAILURE);
+    }
+
+    
+    // read ist id
+    char uid[6];
+    pos = 0;
+
+    c = getchar();
+    if (c == '0') {
+        fprintf(stderr, "invalid ist id");
+        exit(EXIT_FAILURE);
+    }
+    else  {
+        uid[pos++] = c;
+    }
+
+    int size = 1;
+    while ((c = getchar()) != ' ') {
+        uid[pos++] = c;
+        size++;
+    }
+    if (size != 5) {
+        fprintf(stderr, "invalid ist id");
+        exit(EXIT_FAILURE);
+    }
+    uid[pos++] = '\0';
+
+    // read password
+    char password[9];
+    pos = 0;
+
+    size = 0;
+    while ((c = getchar()) != '\n') {
+        password[pos++] = c;
+        size++;
+    }
+    if (size != 8) {
+        fprintf(stderr, "invalid password");
+        exit(EXIT_FAILURE);
+    }
+    password[pos++] = '\0';
+
+    // REG UID pass PDIP PDport
+    char message[45];
+
+    if (sprintf(message, "REG %s %s %s %s\n", uid, password, pd_ip, pd_port) < 0) {
+        fprintf(stderr, "ERRO");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%s\n", message);
+
     int fd,errcode;
     ssize_t n;
     socklen_t addrlen;
@@ -57,22 +122,22 @@ int main(int argc, char **argv) {
     struct sockaddr_in addr;
     char buffer[128];
 
-    fd=socket(AF_INET,SOCK_DGRAM,0); //UDP socket
-    if(fd==-1) /*error*/exit(1);
+    fd = socket(AF_INET,SOCK_DGRAM,0); //UDP socket
+    if(fd == -1) exit(1);
 
     memset(&hints,0,sizeof hints);
     hints.ai_family = AF_INET; //IPv4
     hints.ai_socktype = SOCK_DGRAM; //UDP socket
 
     errcode = getaddrinfo(as_ip, as_port, &hints, &res) ;
-    if(errcode!=0) /*error*/ exit(1);
+    if(errcode!=0)  exit(1);
 
-    n = sendto(fd,"Hello!\n",7,0,res->ai_addr,res->ai_addrlen);
-    if(n==-1) /*error*/ exit(1);
+    n = sendto(fd, message, strlen(message), 0, res->ai_addr, res->ai_addrlen);
+    if(n==-1) exit(1);
 
     addrlen = sizeof(addr);
-    n = recvfrom (fd,buffer,128,0, (struct sockaddr*)&addr,&addrlen);
-    if(n == -1) /*error*/ exit(1);
+    n = recvfrom (fd, buffer, 128,0, (struct sockaddr*)&addr, &addrlen);
+    if(n == -1) exit(1);
 
     write(1,"echo: ",6); write(1,buffer,n);
 

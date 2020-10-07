c97b8816d476b9650259400c712cb51e22053ff3
MiguelJardim <migueljardim2000@gmail.com>
2020-10-07 14:58:32 +0100

    getopt funciona

--- src/pd.c
+++ src/pd.c
@@ -227,14 +227,6 @@ int main(int argc, char **argv) {
         case 'p':
             strcpy(as_port, optarg);
             break;
-        case '?':
-            if (optopt == 'c')
-                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-            else if (isprint (optopt))
-                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-            else
-                fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
-            return 1;
         default:
             abort();
         }
